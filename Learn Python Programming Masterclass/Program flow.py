97ccda88082c4f58fb101e14e3f1bb2d74db763e
tommyst <tsisak90@gmail.com>
2019-05-05 14:36:06 +0200

    Ready up to 33.

--- Learn Python Programming Masterclass/Program flow.py
+++ Learn Python Programming Masterclass/Program flow.py
@@ -48,11 +48,40 @@
 #         print("Sorry, you have not guessed correctly.")
 # else:
 #     print("You got it first time!")
-age = int(input("How old are you? "))
+#age = int(input("How old are you? "))
+#
+#if (age >= 16) and (age <= 65):
+#    print("Have a good day at work!")
+#elif (10 <= age <= 16):
+#    print("You should be at school now!")
+#else:
+#    print("Whatever")
+#
+#x = "False"
+#if x:
+#    print("x is true")
+#
+#x = input("Please enter something: ")
+#if x:
+#    print("You've entered: {0}".format(x))
+#else:
+#    print("You did not enter anything!")
+#
+#print(not False)
+#print(not True)
+#
+#age = int(input("How old are you? "))
+#if not(age < 18):
+#    print("You're old enough to vote!")
+#    print("Please put an x in the box")
+#else:
+#    print("Please come back in {0} years.".format(18 - age))
 
-if (age >= 16) and (age <= 65):
-    print("Have a good day at work!")
-elif (10 <= age <= 16):
-    print("You should be at school now!")
+parrot = "Norwegian Blue"
+
+letter = input("Enter a character: ")
+
+if letter in parrot:
+    print("Give me an {}, Bob.".format(letter))
 else:
-    print("Whatever")
+    print("I don't need that letter.")
